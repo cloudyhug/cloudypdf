afa86789f926f78ab9a2748baa0d129bd70e5dcd
cloudyhug <cloudyhug@protonmail.ch>
2019-07-06 14:33:17 +0200

    cleaning the code, adding a few comments, adding examples to readme, and mentioning the camlpdf required package

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # cloudypdf
-Quick tool to merge or split PDF files
+Small tool to merge or split PDF files on the command line
 
-# How to build
-You need the opam package manager, a recent version of OCaml, and the dune build system installed on your machine. Run `dune build main.exe` and you will get the executable in `_build/default/main.exe`.
+## How to build
+You need the opam package manager, a recent version of OCaml, and the dune build system installed on your machine. Install the `camlpdf` package with `opam install camlpdf`. Run `dune build main.exe` and you will get the executable in `_build/default/main.exe`.
+
+## Examples
+
+`main.exe a.pdf 1:2:4 b.pdf` : takes pages 1, 2, 4 from `a.pdf` to `b.pdf`.
+`main.exe a.pdf 1:5-8 b.pdf` : takes pages 1, 5, 6, 7, 8 from `a.pdf` to `b.pdf`.
+`main.exe a.pdf 4- b.pdf` : takes all the pages counting from page 4 (included) in `a.pdf` to `b.pdf`.
+`main.exe a.pdf -7 b.pdf` : takes all the pages before page 7 (included) in `a.pdf` to `b.pdf`.
+`main.exe a.pdf 1:5 b.pdf -3:6:8-10:12- c.pdf` : takes pages 1 and 5 from `a.pdf`, pages 1, 2, 3, 6, 8, 9, 10, and all the pages after 12 in `b.pdf`, saves in `c.pdf`.
--- main.ml
+++ main.ml
@@ -1,47 +1,51 @@
+(* a -- b = [a; a + 1; a + 2; ...; b] *)
 let ( -- ) a b = List.init (b - a + 1) (fun x -> x + a)
 
 type range_element =
-  | SinglePage of int
-  | LeftBound of int
-  | RightBound of int
-  | DoubleBound of int * int
-  | FullPdf
+  | SinglePage  of int         (* 1 *)
+  | LeftBound   of int          (* 1- *)
+  | RightBound  of int         (* -7 *)
+  | DoubleBound of int * int  (* 3-5 *)
+  | FullPdf                   (* - *)
 
-let parse_range str =
+(* A range is a list of range elements separated by ':' (ex: '1:2:5-6:9-') *)
+let range_of_string str =
   try
-    let range_of_group group_str =
+    let range_element_of_string group_str =
       match String.split_on_char '-' group_str with
-      | [""; ""] -> FullPdf
-      | [""; end_page] -> RightBound (int_of_string end_page)
-      | [start_page; ""] -> LeftBound (int_of_string start_page)
+      | [""; ""]               -> FullPdf
+      | [""; end_page]         -> RightBound (int_of_string end_page)
+      | [start_page; ""]       -> LeftBound (int_of_string start_page)
       | [start_page; end_page] -> DoubleBound (int_of_string start_page, int_of_string end_page)
-      | [page] -> SinglePage (int_of_string page)
-      | _ -> failwith ""
+      | [page]                 -> SinglePage (int_of_string page)
+      | _                      -> failwith ""
     in
     String.split_on_char ':' str
-    |> List.map range_of_group
+    |> List.map range_element_of_string
   with Failure _ -> failwith ("invalid range '" ^ str ^ "'")
 
+(* Creates tuples with a filename and a page range from the arguments *)
 let parse_args args args_length =
   let rec parse_args_aux inputs i n =
     if i = n then List.rev inputs else
     if i = n - 1 then List.rev ((args.(i), None) :: inputs) else
     let input_file = args.(i) in
-    match parse_range args.(i + 1) with
-    | range -> parse_args_aux ((input_file, Some range) :: inputs) (i + 2) n
+    match range_of_string args.(i + 1) with
+    | range               -> parse_args_aux ((input_file, Some range) :: inputs) (i + 2) n
     | exception Failure _ -> parse_args_aux ((input_file, None) :: inputs) (i + 1) n
   in
   parse_args_aux [] 1 args_length
 
+(* Processes a tuple, reading the PDF data in the file whose name is provided, and preparing the page list *)
 let prepare_pdf (filename, range_opt) =
   let pdf = Pdfread.pdf_of_file None None filename in
   let number_of_pages = Pdfpage.endpage pdf in
   let range_element_to_numbers = function
-    | SinglePage p -> [p]
-    | LeftBound start_page -> start_page -- number_of_pages
-    | RightBound end_page -> 1 -- end_page
+    | SinglePage p                       -> [p]
+    | LeftBound start_page               -> start_page -- number_of_pages
+    | RightBound end_page                -> 1          -- end_page
     | DoubleBound (start_page, end_page) -> start_page -- end_page
-    | FullPdf -> 1 -- number_of_pages
+    | FullPdf                            -> 1          -- number_of_pages
   in
   let pages_to_cut =
     match range_opt with
@@ -50,6 +54,7 @@ let prepare_pdf (filename, range_opt) =
   in
   (pdf, List.flatten pages_to_cut)
 
+(* Help message *)
 let usage =
   "Usage: " ^ Sys.argv.(0) ^ " \
 (input_file pages?)+ output_file
@@ -63,9 +68,9 @@ pages:
 
 let () =
   match Array.length Sys.argv with
-  | 1 | 2 -> print_endline usage
+  | 1 | 2 -> print_endline usage (* at least an input file and an output file are required *)
   | n ->
-    let output_file = Sys.argv.(n - 1) in
+    let output_file = Sys.argv.(n - 1) in (* the output file is always the last argument *)
     let args = parse_args Sys.argv (n - 1) in
     try
       let pdfs, ranges = List.map prepare_pdf args |> List.split in
